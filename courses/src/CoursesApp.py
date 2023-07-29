d0e7a15a1d4b832be9d13bc0f87633bcc3456300
PatrickMykoda <p.mykoda@uniandes.edu.co>
2023-07-29 18:26:47 -0500

    Arreglos: Conexión a Cloud SQL

--- courses/src/CoursesApp.py
+++ courses/src/CoursesApp.py
@@ -1,18 +1,18 @@
 from flask import Flask, jsonify, Response
-from .modelos import db
+from modelos import db
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
-from .vistas.CoursesLogic import Courses, Course, GetNumCoursesByTimeRange, SoftDelete, RestoreDeletedCourse, \
+from vistas.CoursesLogic import Courses, Course, GetNumCoursesByTimeRange, SoftDelete, RestoreDeletedCourse, \
     GetNumCoursesByTimeRange, HealthCheck
 
 app = Flask(__name__)
-app.config.from_object("src.config.Config")
+app.config.from_object("config.Config")
 
 
-#@app.before_first_request
+@app.before_first_request
 
-#def create_tables():
-# db.create_all()
+def create_tables():
+    db.create_all()
 
 
 db.init_app(app)
--- courses/src/config.py
+++ courses/src/config.py
@@ -1,3 +1,3 @@
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:admin@35.188.80.59:5432/postgres"
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:admin@35.192.200.221:5432/postgres"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
