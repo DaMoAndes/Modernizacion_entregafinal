df0dc3023b01b342e7ab9ffb8c2bd5c9d7c034dd
ChristianBorrasTorres <c.borras@uniandes.edu.co>
2023-07-29 00:51:00 -0500

    url change

--- courses/src/config.py
+++ courses/src/config.py
@@ -2,9 +2,6 @@ import os
 from datetime import datetime
 
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///test.db")
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:admin@35.188.80.59:5432/postgres"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
