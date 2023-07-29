fab609c05a41f01ef87c814d725650c73b186f3f
Christian Borrás <c.borras@uniandes.edu.co>
2023-07-29 11:19:36 -0500

    Delete config.py

--- courses/src/config.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-
-class Config(object):
-    SQLALCHEMY_DATABASE_URI = ("postgresql://postgres:admin@35.188.80.59:5432/postgres")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'secret_key'
-    JWT_ACCESS_TOKEN_EXPIRES = 3600
